5243ac53372698153eccb851dc38d3d4d355dd2c
Max <maks.potapenko@gmail.com>
2024-03-19 13:11:51 +0200

    added a thing that shows you have many goods are in your cart

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -8,7 +8,7 @@ function Dashboard() {
   return (
     <>
       <Head>
-        <title>Legoland | {shouldLoadContent ? 'Главная' : ''}</title>
+        <title>LegoLand | {shouldLoadContent ? 'Главная' : ''}</title>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
