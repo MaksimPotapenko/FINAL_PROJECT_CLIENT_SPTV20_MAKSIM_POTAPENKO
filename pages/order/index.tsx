d6eedf112b5b2519ae428587dcf4333142f17c30
Max <maks.potapenko@gmail.com>
2024-04-22 15:50:13 +0300

    added checkout and payment, all working, payment logs are saved in yookasssa acc

---
+++ pages/order/index.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Layout from '@/components/layout/Layout'
+import useRedirectByUserCheck from '@/hooks/useRedirectByUserCheck'
+import OrderPage from '@/components/templates/OrderPage/OrderPage'
+
+function Order() {
+  const { shouldLoadContent } = useRedirectByUserCheck()
+
+  return (
+    <>
+      <Head>
+        <title>LegoLand | {shouldLoadContent ? 'Оформление заказа' : ''}</title>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/svg" sizes="32x32" href="/img/logo.svg" />
+      </Head>
+      {shouldLoadContent && (
+        <Layout>
+          <main>
+            <OrderPage />
+            <div className="overlay" />
+          </main>
+        </Layout>
+      )}
+    </>
+  )
+}
+
+export default Order
